cf4246321ea65d859cfdd06b452fb909fbc7e44d
2B键盘 <2968640488@qq.com>
2022-07-29 11:19:33 +0800

    RM-删除测试代码

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@
 
 int main(int argc, char **argv)
 {
-    vmarchtools::printf_to_stdout("%d\n", vmarchtools::value_of<int>("123"));
-    vmarchtools::printf_to_stdout("%f\n", vmarchtools::value_of<float>("3.1415926"));
-//    vmarchcmd_main(argc, argv);
+    vmarchcmd_main(argc, argv);
     return 0;
 }
