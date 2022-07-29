bfd260d69150898bd5acc65b5b12efd1b0693b5c
2B键盘 <2968640488@qq.com>
2022-07-29 17:09:40 +0800

    RM-删除vmarchtools中的printf_to_stdout()声明

--- vmarchtools.h
+++ vmarchtools.h
@@ -14,7 +14,6 @@ namespace vmarchtools {
     std::string vfmt(const std::string &__fmt, va_list __va); /* va_list格式化 */
     std::string fmt(const std::string &__fmt, ...); /* 字符串格式化 */
     void verror(const std::string &__fmt, ...); /* 程序中断错误 */
-    void printf_to_stdout(const std::string &__fmt, ...); /* 打印标准 */
     bool is_number(const std::string &str); /* 判断字符串是不是数字 */
     void fread_all(const std::string &path, std::string *p_buf); /* 可能不适用于vfs文件系统中的文件 */
     std::vector<std::string> split(const std::string &str, const std::string &delim); /* 分割字符串 */
